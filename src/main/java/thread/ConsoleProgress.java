93402d1e695ab089cc361674418952929a16a2c6
SavvaMey <savva28031997@gmail.com>
2021-02-15 23:55:27 +0300

    3. Прерывание нити [#474478]

---
+++ src/main/java/thread/ConsoleProgress.java
@@ -0,0 +1,28 @@
+package thread;
+
+public class ConsoleProgress implements Runnable {
+    String[] process = {"-", "\\", "|", "/"};
+    @Override
+    public void run() {
+        int count = 0;
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                Thread.sleep(200);
+                System.out.print("\r load: " + process[count++]);
+                if (count > 3) {
+                    count = 0;
+                }
+            } catch (InterruptedException e) {
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(1000); /* симулируем выполнение параллельной задачи в течение 1 секунды. */
+        progress.interrupt();
+        System.out.println( progress.getState());
+    }
+}
