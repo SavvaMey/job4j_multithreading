0b8ce9e85ba5f1ac0b998d2cb207027006aca1b3
SavvaMey <savva28031997@gmail.com>
2021-02-15 23:29:00 +0300

    1.1. Состояние нити. [#474474]

---
+++ src/main/java/thread/Wget.java
@@ -0,0 +1,21 @@
+package thread;
+
+public class Wget {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(
+                () -> {
+                    for (int i = 0; i < 100; i++) {
+                        System.out.print("\rLoading : " + i  + "%");
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+        );
+        thread.start();
+        thread.join();
+        System.out.println("Loaded main");
+    }
+}
