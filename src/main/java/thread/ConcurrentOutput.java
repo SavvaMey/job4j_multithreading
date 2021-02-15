1205838de86c4876838ca33d16233e8b6a951af7
SavvaMey <savva28031997@gmail.com>
2021-02-15 22:14:23 +0300

    dssd

---
+++ src/main/java/thread/ConcurrentOutput.java
@@ -0,0 +1,20 @@
+package thread;
+
+public class ConcurrentOutput {
+    public static void main(String[] args) {
+        Thread another = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        another.start();
+        System.out.println(Thread.currentThread().getName());
+        Thread anotherOne = new Thread(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        System.out.println(Thread.currentThread().getName());
+                    }
+                }
+        );
+        anotherOne.start();
+    }
+}
