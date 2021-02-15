cecd54d06f1c26a2df867606573bdb7aaf4b6ee9
SavvaMey <savva28031997@gmail.com>
2021-02-15 23:16:34 +0300

    1.1. Состояние нити. [#474474]

---
+++ src/main/java/thread/ThreadState.java
@@ -0,0 +1,25 @@
+package thread;
+
+import jdk.swing.interop.SwingInterOpUtils;
+
+public class ThreadState {
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> System.out.println("first hello")
+        );
+        Thread second = new Thread(
+                () -> System.out.println("second hello")
+        );
+        System.out.println(first.getState());
+        first.start();
+        second.start();
+        while ((first.getState() != Thread.State.TERMINATED) &&
+                (second.getState() != Thread.State.TERMINATED)) {
+            System.out.println(first.getState() + " first");
+            System.out.println(second.getState() + " second");
+        }
+        System.out.println(first.getState() + " FIRST");
+        System.out.println(second.getState()+ " SECOND");
+        System.out.println("main end");
+    }
+}
