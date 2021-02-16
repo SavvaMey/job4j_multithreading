0f912000d401af094ba3e0fa0e4afb77013d1669
SavvaMey <savva28031997@gmail.com>
2021-02-16 22:45:40 +0300

    0. Управление нитью через wait. [#474471]

---
+++ src/main/java/waitnotify/CountBarrier.java
@@ -0,0 +1,62 @@
+package waitnotify;
+
+public class CountBarrier {
+    private final Object monitor = this;
+
+    private final int total;
+
+    private int count = 0;
+
+    public CountBarrier(final int total) {
+        this.total = total;
+    }
+
+    public void count() {
+        synchronized (monitor) {
+            count++;
+            if (count == total) {
+                monitor.notifyAll();
+            }
+        }
+    }
+
+    public void await() {
+        synchronized (monitor) {
+            while (count != total) {
+                try {
+                    monitor.wait();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        CountBarrier barrier = new CountBarrier(10);
+        Thread master = new Thread(
+                () -> {
+                    for (int i = 0; i <= 10; i++) {
+                        barrier.count();
+                        try {
+                            Thread.sleep(200);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        System.out.println("hello from counter +" + i);
+
+                    }
+                },
+                "counter"
+        );
+        Thread slave = new Thread(
+                () -> {
+                    barrier.await();
+                    System.out.println("no wait anymore");
+                },
+                "Slave"
+        );
+        master.start();
+        slave.start();
+    }
+}
