b68a5296f5f409cf64ee98f4ac73599aeb8169fe
SavvaMey <savva28031997@gmail.com>
2021-02-22 17:33:08 +0300

    test

--- src/main/java/git/Hello.java
+++ src/main/java/git/Hello.java
@@ -1,6 +1,6 @@
 package git;
 
-public class hello {
+public class Hello {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("new changes");
--- src/main/java/git/Otkatizmeneniy.java
+++ src/main/java/git/Otkatizmeneniy.java
@@ -1,6 +1,6 @@
 package git;
 
-public class otkatizmeneniy {
+public class Otkatizmeneniy {
     public static void main(String[] args) {
         System.out.println("first commit");
         System.out.println("second");
