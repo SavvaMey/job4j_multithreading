ce2d8a4d116037cb6b78565ada0f640b39b51d49
SavvaMey <savva28031997@gmail.com>
2021-02-22 15:46:47 +0300

    3.2. Откат изменений. [#474331]

---
+++ src/main/java/git/otkatizmeneniy.java
@@ -0,0 +1,9 @@
+package git;
+
+public class otkatizmeneniy {
+    public static void main(String[] args) {
+        System.out.println("first commit");
+        System.out.println("second");
+        System.out.println("third");
+    }
+}
